694df8e3ef9be782a3ff94250c7e804e93226bb7
Anand Raj Bind <116335833+AnandRajBind@users.noreply.github.com>
2024-09-04 01:36:15 -0700

    Add files via upload

---
+++ first.java
@@ -0,0 +1,33 @@
+
+public class first {
+    public static void main(String[] args) {
+
+        int rating = 15;
+        switch (rating) {
+            case 1:
+                System.out.println(" Your rating is one ");
+                break;
+
+            case 2:
+                System.out.println(" Your rating is one ");
+                break;
+
+            case 3:
+                System.out.println(" Your rating is one ");
+                break;
+
+            case 4:
+                System.out.println(" Your rating is one ");
+                break;
+
+            case 5:
+                System.out.println(" Your rating is one ");
+                break;
+
+
+                default:
+                System.out.println("invalid Rating ");
+                break;
+        }
+    }
+}
---
+++ input.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class input {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the first value ");
+        int a = sc.nextInt();
+
+        System.out.println("Enter the Second  value ");
+        int b = sc.nextInt();
+
+        System.out.println("your two value addition is "+ (a + b));
+
+    }
+}
