97cde88cfd19e89737b5c9db828798152644fa0f
Anand Raj Bind <116335833+AnandRajBind@users.noreply.github.com>
2024-09-04 01:37:32 -0700

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,14 @@
+import  java.util.Scanner;
+public class factorial {
+    public static void main(String[] args) {
+        
+        int fact =1;
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the num value");
+        int num =sc.nextInt();
+        for(int i=1; i<=num; i++){
+fact=fact*i;
+}
+ System.out.println(fact);
+    }
+}
