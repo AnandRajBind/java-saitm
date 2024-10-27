d6e2d1ef5e66a1384a7a906fec9a3646412850cd
Anand Raj Bind <116335833+AnandRajBind@users.noreply.github.com>
2024-10-27 16:05:16 +0530

    27/10/24

--- Throw_Throwes.java
+++ Throw_Throwes.java
@@ -1,12 +1,22 @@
-class Throw_Throwes{
-    int divide(int a, int b){
-// made by harry 
-
-int result =a/b;
+class Throw_Throwes {
+public static double area(int r){
+double result =Math.PI*r*r;
 return result;
-    }
-    public static void main(String args){
-//Shivam- uses divide function created by harry
-
 }
+
+    public static int divide(int a, int b) throws ArithmeticException {
+        // made by harry
+        int result = a / b;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        // Shivam- uses divide function created by harry
+        try {
+            int c = divide(6, 0);
+            System.out.println(c);
+        } catch (Exception e) {
+            System.out.println("Exception");
+        }
+    }
 }
